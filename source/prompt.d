8110c9d41930c9ed09fc6c600e16a84967ee7ad4
Sahan Fernando <sahan.h.fernando@gmail.com>
2026-06-28 01:44:30 +1000

    Colorize the prompt

--- source/prompt.d
+++ source/prompt.d
@@ -5,6 +5,12 @@ import std.stdio;
 
 import agent_harness.llamacpp_proto;
 
+enum Color { red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36 }
+
+string colorize(string s, Color c) {
+    return format("\x1b[%dm%s\x1b[0m", cast(int) c, s);
+}
+
 // TODO(ccapitalk): This whole stack needs to be rethought a bit later down the line, especially for rewind.
 
 void printChat(LlamaMessage[] messages) {
@@ -16,14 +22,14 @@ void printChat(LlamaMessage[] messages, ref string[string] toolIdToParamMap) {
     foreach (message; messages) {
         switch (message.role) {
         case "system":
-            writeln("System: ", message.content);
+            writeln("System: ".colorize(Color.yellow), message.content);
             break;
         case "user":
-            writeln("User: ", message.content);
+            writeln("User: ".colorize(Color.green), message.content);
             break;
         case "assistant":
             if (message.reasoningContent != "") {
-                writeln("Agent(Reasoning): ", message.reasoningContent);
+                writeln("Agent(Reasoning): ".colorize(Color.red), message.reasoningContent);
             }
             foreach (call; message.toolCalls) {
                 string funcName = call.function_.name;
@@ -31,11 +37,12 @@ void printChat(LlamaMessage[] messages, ref string[string] toolIdToParamMap) {
                 string id = call.id;
                 toolIdToParamMap[id] = format!"%s(%s)"(funcName, params);
             }
-            writeln("Agent: ", message.content);
+            writeln("Agent: ".colorize(Color.blue), message.content);
             break;
         case "tool":
             auto params = message.toolCallId in toolIdToParamMap;
-            writefln!"Tool(%s): %s"(params ? *params : message.toolCallId, message.content);
+            enum fmtString = "Tool".colorize(Color.cyan) ~ ": %s = %s";
+            writefln!fmtString(params ? *params : message.toolCallId, message.content);
             break;
         default:
             writeln(message);
