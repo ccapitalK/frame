ad2697ec8c0868dbd7689f16ba079310d05804fc
Sahan Fernando <sahan.h.fernando@gmail.com>
2026-06-29 11:10:03 +1000

    Add basic example agent that reads a filelist and summarizes them

--- source/app.d
+++ source/app.d
@@ -1,33 +1,88 @@
 module agent_harness.app;
 
+import std.array;
+import std.algorithm;
+import std.exception;
+import std.file;
 import std.stdio;
 
 import agent_harness.agent;
 import agent_harness.prompt;
 import agent_harness.tools;
 
-struct Num2Req {
-    @ToolDoc("First number")
-    double a;
-    @ToolDoc("Second number")
-    double b;
+string[] readFileList(string fileListPath) {
+    return fileListPath.readText().splitter('\n').filter!"a != []".array;
 }
 
-ToolDef[] tools() {
-    return [
-        simpleToolDef!((Num2Req req) => req.a + req.b)("add2", "Add a and b"),
-        simpleToolDef!((Num2Req req) => req.a - req.b)("sub2", "Subtract b from a"),
-    ];
+struct Empty {}
+
+struct ViewRange {
+    @ToolDoc("File to read from")
+    string fileName;
+    @ToolDoc("Offset to read starting from")
+    size_t offset;
+}
+
+struct ViewRangeResponse {
+    string response;
+    size_t actualFileLength;
+    size_t responseLength;
+}
+
+struct AddEntry {
+    @ToolDoc("File to submit summary for")
+    string filename;
+    @ToolDoc("Summary of the file")
+    string summary;
 }
 
 void main(string[] args) {
     string port = "12349";
-    if (args.length > 1) {
-        port = args[1];
-    }
+    auto files = args[1].readFileList();
+    string[string] summaries;
+    auto remainingFiles() => files[].filter!(a => a !in summaries);
+    bool hasSubmitted;
+    auto tools = [
+        simpleToolDef!((Empty _) => files[].filter!(a => a !in summaries).array)
+            ("remainingFiles", "Return list of files yet to be summarized"),
+        simpleToolDef!((ViewRange req) {
+            writeln(req);
+            enforce(files[].any!(a => a == req.fileName));
+            auto text = req.fileName.readText();
+            enforce(text.length >= req.offset);
+            auto end = min(text.length, req.offset + 1024);
+            return ViewRangeResponse(
+                text[req.offset .. end],
+                text.length,
+                end - req.offset,
+            );
+        })("viewRange", "View a range of bytes in a file. Limits to at most 1024 in a single query"),
+        simpleToolDef!((AddEntry req) {
+            enforce(remainingFiles.any!(a => a == req.filename));
+            summaries[req.filename] = req.summary;
+            hasSubmitted = true;
+            return "";
+        })("submitSummary", "Submit a summary of a file"),
+    ];
     auto agent = makeAgent("localhost", port, tools);
-    agent.history ~= [systemPrompt("You are helping me test my llm harness")];
-    agent.history ~= userPrompt("Test the tools extensively, including edge case inputs (what happens if you try to avoid the schema?"
-            ~ " Figure out if the harness is preventing you from breaking away from the schema)");
-    agent.runNormalAgentLoop();
+    agent.history = [systemPrompt(
+        "You are an autonomous agent summarizing code files. You are running fully autonomously, do not prompt the user for more information."
+    )];
+    agent.history ~= userPrompt(
+        "Pick a file to summarize, read through the file making notes to yourself out loud, then submit a summary of the file. "
+    );
+    while (remainingFiles.count > 0) {
+        hasSubmitted = false;
+        agent.printer.rewind(2);
+        while (true && !hasSubmitted) {
+            auto message = agent.invokeAgentResponse();
+            if (message.isEndOfAgentMessageSequence) {
+                break;
+            }
+            agent.printer.printLog();
+        }
+        agent.printer.printLog();
+    }
+
+    writeln(summaries);
 }
